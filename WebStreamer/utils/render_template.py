f55fdc16cff2470da17a93cc38dce815ccd3153d
Streamtg <202310475@issu.edu.do>
2025-08-02 19:24:13 -0400

    Update render_template.py

--- WebStreamer/utils/render_template.py
+++ WebStreamer/utils/render_template.py
@@ -1,56 +1,45 @@
-# This file is a part of TG-FileStreamBot
-
 import os
-import urllib.parse
 import aiofiles
-import logging
+import urllib.parse
 import aiohttp
+import logging
 
-from WebStreamer.vars import Var
 from WebStreamer.bot import StreamBot
+from WebStreamer.vars import Var
 from WebStreamer.utils.human_readable import humanbytes
 from WebStreamer.utils.file_properties import get_file_ids
 from WebStreamer.server.exceptions import InvalidHash
 
-# Ruta a la carpeta raíz de plantillas
-BASE_TEMPLATE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../template'))
+# 🔹 Ajuste para que busque dentro de WebStreamer/template
+BASE_TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), '../template')
 
 async def render_page(message_id, secure_hash):
-    # Obtiene datos del archivo desde Telegram
     file_data = await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(message_id))
     if file_data.unique_id[:6] != secure_hash:
-        logging.debug(f'link hash: {secure_hash} - {file_data.unique_id[:6]}')
-        logging.debug(f"Invalid hash for message with - ID {message_id}")
+        logging.debug(f"Invalid hash for message ID {message_id}")
         raise InvalidHash
 
-    # URL pública que servirá el bot
-    src = urllib.parse.urljoin(Var.URL, f'{secure_hash}{str(message_id)}')
-    media_type = str(file_data.mime_type.split('/')[0].strip())
+    src = urllib.parse.urljoin(Var.URL, f"{secure_hash}{message_id}")
+    media_type = file_data.mime_type.split('/')[0].strip()
 
-    # VIDEO
-    if media_type == 'video':
-        template_path = os.path.join(BASE_TEMPLATE_DIR, 'req.html')
-        async with aiofiles.open(template_path) as r:
-            heading = f'Watch {file_data.file_name}'
-            tag = media_type
-            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
-
-    # AUDIO
-    elif media_type == 'audio':
-        template_path = os.path.join(BASE_TEMPLATE_DIR, 'req.html')
-        async with aiofiles.open(template_path) as r:
-            heading = f'Listen {file_data.file_name}'
-            tag = media_type
-            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
-
-    # DESCARGA
+    if media_type in ['video', 'audio']:
+        template_file = 'req.html'
     else:
-        template_path = os.path.join(BASE_TEMPLATE_DIR, 'dl.html')
-        async with aiofiles.open(template_path) as r:
-            async with aiohttp.ClientSession() as s:
-                async with s.get(src) as u:
-                    heading = f'Download {file_data.file_name}'
-                    file_size = humanbytes(int(u.headers.get('Content-Length')))
-                    html = (await r.read()) % (heading, file_data.file_name, src, file_size)
+        template_file = 'dl.html'
+
+    template_path = os.path.join(BASE_TEMPLATE_DIR, template_file)
+    async with aiofiles.open(template_path, mode='r') as f:
+        template = await f.read()
+
+    if media_type in ['video', 'audio']:
+        heading = f"{'Watch' if media_type == 'video' else 'Listen'} {file_data.file_name}"
+        html = template.replace('tag', media_type) % (heading, file_data.file_name, src)
+    else:
+        heading = f"Download {file_data.file_name}"
+        async with aiohttp.ClientSession() as session:
+            async with session.head(src) as resp:
+                size_bytes = int(resp.headers.get('Content-Length', 0))
+                file_size = humanbytes(size_bytes)
+        html = template % (heading, file_data.file_name, src, file_size)
 
     return html
