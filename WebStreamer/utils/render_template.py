e889542201701a95ebf940a1893e978bfa55ec53
Streamtg <202310475@issu.edu.do>
2025-08-02 18:17:17 -0400

    Update render_template.py

--- WebStreamer/utils/render_template.py
+++ WebStreamer/utils/render_template.py
@@ -1,38 +1,46 @@
-# This file is a part of TG-FileStreamBot
-
-import urllib.parse
+import os
 import aiofiles
-import logging
+import urllib.parse
 import aiohttp
-from WebStreamer.vars import Var
+import logging
+
 from WebStreamer.bot import StreamBot
+from WebStreamer.vars import Var
 from WebStreamer.utils.human_readable import humanbytes
 from WebStreamer.utils.file_properties import get_file_ids
 from WebStreamer.server.exceptions import InvalidHash
 
+BASE_TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), '../../template')
 
 async def render_page(message_id, secure_hash):
-    file_data=await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(message_id))
+    file_data = await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(message_id))
     if file_data.unique_id[:6] != secure_hash:
-        logging.debug(f'link hash: {secure_hash} - {file_data.unique_id[:6]}')
-        logging.debug(f"Invalid hash for message with - ID {message_id}")
+        logging.debug(f"Invalid hash for message ID {message_id}")
         raise InvalidHash
-    src = urllib.parse.urljoin(Var.URL, f'{secure_hash}{str(message_id)}')
-    if str(file_data.mime_type.split('/')[0].strip()) == 'video':
-        async with aiofiles.open('WebStreamer/template/req.html') as r:
-            heading = 'Watch {}'.format(file_data.file_name)
-            tag = file_data.mime_type.split('/')[0].strip()
-            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
-    elif str(file_data.mime_type.split('/')[0].strip()) == 'audio':
-        async with aiofiles.open('WebStreamer/template/req.html') as r:
-            heading = 'Listen {}'.format(file_data.file_name)
-            tag = file_data.mime_type.split('/')[0].strip()
-            html = (await r.read()).replace('tag', tag) % (heading, file_data.file_name, src)
+
+    src = urllib.parse.urljoin(Var.URL, f"{secure_hash}{message_id}")
+    media_type = file_data.mime_type.split('/')[0].strip()
+
+    if media_type in ['video', 'audio']:
+        template_file = 'req.html'
     else:
-        async with aiofiles.open('WebStreamer/template/dl.html') as r:
-            async with aiohttp.ClientSession() as s:
-                async with s.get(src) as u:
-                    heading = 'Download {}'.format(file_data.file_name)
-                    file_size = humanbytes(int(u.headers.get('Content-Length')))
-                    html = (await r.read()) % (heading, file_data.file_name, src, file_size)
+        template_file = 'dl.html'
+
+    template_path = os.path.join(BASE_TEMPLATE_DIR, template_file)
+    async with aiofiles.open(template_path, mode='r') as f:
+        template = await f.read()
+
+    heading = ''
+    file_size = ''
+    if media_type in ['video', 'audio']:
+        heading = f"{'Watch' if media_type == 'video' else 'Listen'} {file_data.file_name}"
+        html = template.replace('tag', media_type) % (heading, heading, src)
+    else:
+        heading = f"Download {file_data.file_name}"
+        async with aiohttp.ClientSession() as session:
+            async with session.head(src) as resp:
+                size_bytes = int(resp.headers.get('Content-Length', 0))
+                file_size = humanbytes(size_bytes)
+        html = template % (heading, heading, file_data.file_name, file_size, src)
+
     return html
